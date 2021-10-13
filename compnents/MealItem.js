38907b0e36a1ef442946dfdf4143e1aff0b50d2c
arfin <arfinchowdhuryarif7@gamil.com>
2021-10-13 17:39:05 +0600

    130. Rendering a Meals List

---
+++ compnents/MealItem.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Text,
+     View,
+      StyleSheet,
+     TouchableOpacity, ImageBackground } from 'react-native'
+
+
+const MealItem = props => {
+    // console.log(props)
+    return (
+        <View style={styles.mealItem}>
+            <TouchableOpacity onPress={props.onSelectMeal}>
+            <View>
+                <View style={{...styles.mealRow,...styles.mealHeader}}>
+                <ImageBackground source={{uri:props.image}} style={styles.bgImage}>
+                   <View style={styles.titleContainer}>
+                   <Text style={styles.bgtextStyle}>{props.title}</Text>
+                   </View>
+                </ImageBackground>
+                </View>
+                <View style={{...styles.mealRow,...styles.mealDetails}}>
+                    <Text style={styles.textStyle}>{props.duration}m</Text>
+                    <Text style={styles.textStyle}>{props.complexity}</Text>
+                    <Text style={styles.textStyle}>{props.affordability}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    mealItem:{
+        height: 200,
+        width:'100%',
+        backgroundColor:'#f5f5f5',
+        borderRadius:10,
+        overflow:'hidden',
+    },
+    textStyle:{
+        color:'black',
+        
+    },
+    bgtextStyle:{
+        fontSize:22,
+        color:'white',
+        
+        textAlign:'center',
+    },
+    mealRow:{
+        flexDirection:'row',
+       
+    },
+    mealHeader:{
+        height: '85%',
+    },
+    mealDetails:{
+            paddingHorizontal: 10,
+            justifyContent: 'space-between',
+            alignItems:'center',
+            height: '15%',
+    },
+    bgImage:{
+        height: '100%',
+        width:'100%',
+        justifyContent:'flex-end'
+    },
+    titleContainer:{
+        backgroundColor:'rgba(0,0,0,0.5)',
+        fontFamily:'DancingScript-SemiBold',
+        paddingVertical:5,
+        paddingHorizontal: 12,
+    }
+})
+
+export default MealItem;
--- screens/CategoriMealScreen.js
+++ screens/CategoriMealScreen.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button,Platform  } from 'react-native';
-import { CATEGORIES } from '../data/dummy-data';
+import { StyleSheet, Text, View, Button, Platform, FlatList } from 'react-native';
+import MealItem from '../compnents/MealItem';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 
-const CategoriMealScreen = (props) =>{
+const CategoriMealScreen = (props) => {
+
     
-    const categoryId = props.navigation.getParam('categoryId');
     // console.log("categoryId==============",categoryId)
-
-    const selectedCategory = CATEGORIES.find(category =>category.id === categoryId);
+    // const selectedCategory = CATEGORIES.find(category =>category.id === categoryId);
     // console.log("selected Category", selectedCategory);
+
+    const renderMealItem = (itemData) => {
+        return <MealItem title={itemData.item.title} 
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity.toUpperCase()}
+        affordability={itemData.item.affordability.toUpperCase()}
+        image={itemData.item.imageUrl}
+        onSelectMeal={()=>{}}/>
+        
+    }
+    const categoryId = props.navigation.getParam('categoryId');
+    const displayedMeals = MEALS.filter(
+        (meal) => meal.categoryIds.indexOf(categoryId) >= 0
+    );
 
     return (
         <View style={styles.screen}>
-            <Text>Hey This is CategoriMeal Screen </Text>
+            {/* <Text>Hey This is CategoriMeal Screen </Text>
             <Text>{selectedCategory.title} </Text>
             <Button title="Go to MealDetail" onPress={()=>props.navigation.navigate('MealDetail')}/>
-            <Button title="Go to Meal" onPress={()=>props.navigation.pop()}/>
+            <Button title="Go to Meal" onPress={()=>props.navigation.pop()}/> */}
+            <FlatList data={displayedMeals}
+                keyExtractor={(item, index) => item.id}
+                renderItem={renderMealItem}
+                style={{width:'100%', padding:10}}
+            />
         </View>
+
     )
 }
 
@@ -25,23 +45,26 @@ CategoriMealScreen.navigationOptions = (navigationData) => {
     // console.log(navigationData);
     const categoryId = navigationData.navigation.getParam('categoryId');
     // console.log(categoryId)
-    const selectedCategory = CATEGORIES.find(category =>category.id === categoryId);
+    const selectedCategory = CATEGORIES.find(category => category.id === categoryId);
     return {
         headerTitle: selectedCategory.title,
         // headerStyle:{
         //     backgroundColor:Platform.OS === 'android' ? selectedCategory.color : 'black'
-           
+
         // },
     };
-   
+
 }
 
 
 const styles = StyleSheet.create({
-    screen:{
+    screen: {
         flex: 1,
-        alignItems:'center',
-        justifyContent:'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonContainer:{
+        margin: 10,
     }
 })
 
